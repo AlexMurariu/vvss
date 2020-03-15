2cf2c81643b37cc52670646db028326edd44787a
MuBirhan <birhan.muradasil@rodeapps.com>
2020-03-15 22:46:47 +0200

    Add Test

--- src/test/java/Lab3VVSS/AppTest.java
+++ src/test/java/Lab3VVSS/AppTest.java
@@ -1,20 +1,43 @@
 package Lab3VVSS;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import Lab3VVSS.Exceptions.ValidatorException;
+import Lab3VVSS.Repository.XMLFileRepository.StudentXMLRepo;
+import Lab3VVSS.Service.XMLFileService.StudentXMLService;
+import Lab3VVSS.Validator.StudentValidator;
 import org.junit.Test;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
+public class AppTest {
+
     @Test
-    public void testShouldAnswerWithTrueTest()
-    {
-        assertTrue( true );
+    public void testAddStudentValidInput() throws ValidatorException {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo xmlRepo = new StudentXMLRepo(studentValidator, "StudentiXML.xml");
+        StudentXMLService xmlService = new StudentXMLService(xmlRepo);
+        String[] params = {"0", "Birhan", "935", "mbie2368@scs.ubbcluj.ro", "Radu Dragos"};
+        xmlService.add(params);
+        assertEquals(xmlRepo.size(), 1);
+    }
+
+    @Test
+    public void testAddStudentInvalidGroupInput() {
+
+        StudentValidator studentValidator = new StudentValidator();
+        StudentXMLRepo xmlRepo = new StudentXMLRepo(studentValidator, "StudentiXML.xml");
+        StudentXMLService xmlService = new StudentXMLService(xmlRepo);
+        String[] params = {"0", "Birhan", "No bueno", "mbie2368@scs.ubbcluj.ro", "Radu Dragos"};
+        try {
+            xmlService.add(params);
+            fail("Rip");
+        } catch (ValidatorException exception) {
+
+        }
     }
 }
